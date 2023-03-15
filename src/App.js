230cc74114b2f8a043e79665db5d4afbae462edb
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-03-15 14:17:23 -0400

    In Products component consuming data and refactor to getting data solely from Context API

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+
+	// console.log(ProductContext);
 
 	const addItem = item => {
 		// console.log(item);
@@ -22,9 +27,11 @@ function App() {
 			<Navigation cart={cart} />
 
 			{/* Routes */}
+			<ProductContext.Provider value={{ products, addItem }}>
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
+			</ProductContext.Provider>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
