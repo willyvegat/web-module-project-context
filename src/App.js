597530d805557061be9cefef509c1b33dbbf7661
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-03-15 23:29:30 -0400

    imported CartContext to APP, wrap the provider and simplified Navigation and ShoppingCart, have a few errors because of not passsing props but will resolve on the next commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -24,18 +25,20 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<CartContext.Provider value={cart}>
+				<Navigation />
+				
+				{/* Routes */}
+				<ProductContext.Provider value={{ products, addItem }}>
+				<Route exact path="/">
+					<Products />
+				</Route>
+				</ProductContext.Provider>
 
-			{/* Routes */}
-			<ProductContext.Provider value={{ products, addItem }}>
-			<Route exact path="/">
-				<Products />
-			</Route>
-			</ProductContext.Provider>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+				<Route path="/cart">
+					<ShoppingCart />
+				</Route>
+			</CartContext.Provider>
 		</div>
 	);
 }
